d9685d3c984ffe172fc71e57c83efa549f7a27cf
Anders Nissen <andnis@gmail.com>
2016-08-17 22:21:07 +0200

    Clean-up

--- src/Generator.hx
+++ src/Generator.hx
@@ -10,13 +10,7 @@ typedef Page = {
     filepath :FilePath,
     content :String,
     data :Dynamic,
-    // subpages :Array<Page>
 }
-
-// enum Tree {
-//     Directory(path :FilePath, content: Array<Tree>);
-//     Resource(path :FilePath /* type enum + data ? */);
-// }
 
 abstract FilePath(String) from String to String {
     public inline function new(path :String) { this = path; }
@@ -27,29 +21,6 @@ abstract FilePath(String) from String to String {
 }
 
 class Generator {
-    // public static function get_page_heirarchy(sitemap :Generator.Tree) :Array<Path> {
-    //     return switch (sitemap) {
-    //         case Resource(path): [{
-    //             filepath: path,
-    //             content: Markdown.markdownToHtml(File.getContent(filepath)),
-    //             (FileSystem.exists(filepath.replaceExtension('json')) ? parse_json_data(File.getContent(filepath.replaceExtension('json'))) : null)
-    //         }];
-    //         case Directory(path, list): var files = []; for (l in list) files = files.concat(get_files(l)); files;
-    //     }
-    // }
-
-    public static function do_everything(contentPath :String, handler :Page->Void) {
-        trace('Load layouts');
-        for (layout in Generator.get_files(contentPath, ~/[.]mtt$/)) {
-            trace('Initializing layout $layout');
-            Template.fromFile(layout);
-        }
-
-        for (page in get_pages(contentPath, ~/[.]md$/)) {
-            handler(page);
-        }
-    }
-
     public static function initialize_templates(dir :FilePath, regex :EReg) {
         for (filepath in get_files(dir, regex)) Template.fromFile(filepath);
     }
@@ -59,11 +30,6 @@ class Generator {
         var template = Template.fromFile(layoutPath);
         File.saveContent(outputPath, template.execute(data));
     }
-
-    // public static function output_resource(path :FilePath, outputPath :FilePath) {
-    //     if (!FileSystem.exists(outputPath.withoutFileName())) FileSystem.createDirectory(outputPath.withoutFileName());
-    //     File.copy(path, outputPath);
-    // }
 
     public static function to_markdown(content :String) :String {
         var parser = new commonmark.Parser();
@@ -79,22 +45,6 @@ class Generator {
             data: (FileSystem.exists(filepath.replaceExtension('json')) ? parse_json_data(File.getContent(filepath.replaceExtension('json'))) : null)
         } ];
     }
-
-    // public static function sitemap(path :FilePath, regex :EReg) :Tree {
-    //     if (!FileSystem.isDirectory(path)) return Resource(path);
-    //     // return Directory(path, [ for (p in FileSystem.readDirectory(path)) sitemap('$path/$p'.replace('//', '/')) ]);
-    //     var resources = [];
-    //     var directories = [];
-    //     for (file in FileSystem.readDirectory(path)) {
-    //         var p = '$path/$file'.replace('//', '/');
-    //         if (FileSystem.isDirectory(p)) {
-    //             directories.push(p);
-    //         } else if (regex.match(p)) {
-    //             resources.push(p);
-    //         }
-    //     }
-    //     return Directory(path, [ for (p in resources.concat(directories)) sitemap(p, regex) ]); // resources first, then directories
-    // }
 
     static public function get_files(dir :FilePath, regex :EReg, ?reverseOrder :Bool = false) :Array<FilePath> {
         var files = [];
@@ -118,42 +68,3 @@ class Generator {
         return haxe.Json.parse(content);
     }
 }
-
-/*
-class MyGenerator {
-    var contentPath = 'assets/content/';
-
-    public function new(contentPath :String, outputPath) {
-        this.contentPath = contentPath;
-        Generator.initialize_templates(contentPath, ~/[.]mtt$/);
-    }
-
-    public function do_it() {
-        var pages = Generator.get_pages(contentPath, ~/[.]md$/); // maybe return a heirarchical structure instead, Generator.get_resources()
-        trace(pages.length + ' pages');
-
-        for (page in pages) {
-            // page = minify(extract_data(remove_comments(page));
-            var filename = page.filepath.replaceExtension('html').humanize();
-            var context = get_data_from_markdown(page);
-
-            // var comments = [];
-            // var withoutComments = ~/<!--([\s\S]*?)-->/gm.map(context.content, function(r) {
-            //     comments.push(r.matched(0).replace('<!--', '').replace('-->', '').trim());
-            //     return ''; // remove comments
-            // });
-            // trace('comments: $comments');
-            
-            // var simpleCommentCollector = new EReg('<!--(.*?)-->', 'g');
-            // trace(simpleCommentCollector.match(context.content));
-            // simpleCommentCollector.map(context.content, function(r) {
-            //     var match = r.matched(0);
-            //     trace('simpleCollector match: $match');
-            //     return match;
-            // });
-
-            Generator.output_page(context, contentPath + 'layout/page.mtt', 'output_test/' + filename);
-        }
-    }
-}
-*/
--- src/Main.hx
+++ src/Main.hx
@@ -1,61 +1,23 @@
-package;
+package ;
 
 class Main {
     public static function main() {
-        var contentPath = 'assets/';
+        var contentPath = 'assets/content/';
         Generator.initialize_templates(contentPath, ~/[.]mtt$/);
 
-        // function get_files(sitemap :Generator.Tree) :Array<Generator.FilePath> {
-        //     return switch (sitemap) {
-        //         case Resource(path): [path];
-        //         case Directory(path, list): var files = []; for (l in list) files = files.concat(get_files(l)); files;
-        //     }
-        // }
-
-        // Generator.do_everything(contentPath, function(page) {
-        //     trace(page);    
-        // });
-
-        // var files = Generator.get_files(contentPath, ~/.*/);
-
-        // // var files = get_files(Generator.sitemap(contentPath));
-
-        // trace('Load layouts');
-        // for (f in files) trace(f);
-
-        // trace('Process files');
-        // files.reverse();
-        // for (f in files) trace(f);
-
-        // trace('Process pages');
-        // print_sitemap2(Generator.sitemap(contentPath));
-
-        var pages = Generator.get_pages(contentPath, ~/[.]md$/); // maybe return a heirarchical structure instead, Generator.get_resources()
+        var pages = Generator.get_pages(contentPath, ~/[.]md$/);
         trace(pages.length + ' pages');
 
-        // TODO: Try with planetarium
-        // TODO: Minimize html
-        // TODO: Replace tags (e.g. youtube link with embedded youtube)
-        // TODO: Expose data from pages/* to e.g. index.mtt
-
-        // Steps:
-        //  1. get resources (markdown, json, pictures, etc)
-        //  2. process resources (change markdown, read data from json, etc)
-        //  3. generate html from markdown and data
-        //  4. output html and resources 
-
         for (page in pages) {
-            // page = minify(extract_data(remove_comments(page));
             var filename = page.filepath.replaceExtension('html').humanize();
             var context = get_data_from_markdown(page);
             trace(filename);
-            Generator.output_page(context, contentPath + (filename == 'assets/index.html' ? 'layout/index.mtt' : 'layout/page.mtt'), 'output/' + filename);
+            Generator.output_page(context, contentPath + (filename == 'assets/content/index.html' ? 'layout/index.mtt' : 'layout/page.mtt'), 'output/' + filename);
         }
     }
 
     static function get_data_from_markdown(page :Generator.Page) :Dynamic {
         var headerRegEx = ~/<h1>(.*?)<\/h1>/;
-        
         if (page.data != null) validate_data(page.data);
 
         return {
