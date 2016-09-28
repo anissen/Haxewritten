31023e107cb169a9a23c959a7ed29083ba8cd2b3
Anders Nissen <andnis@gmail.com>
2016-09-28 20:58:46 +0200

    Update

--- src/Main.hx
+++ src/Main.hx
@@ -4,46 +4,16 @@ using StringTools;
 
 class Main {
     public static function main() {
-        // var contentPath = 'assets';
-        Generator.initialize_templates('assets/layout/', ~/[.]mtt$/);
+        Generator.initialize_templates('portfolio/layout/', ~/[.]mtt$/);
 
-        // var pages = Generator.get_pages(contentPath, ~/[.]md$/);
-        // trace(pages.length + ' pages');
 
-        // for (page in pages) {
-        //     var filename = page.filepath.replaceExtension('html').humanize();
-        //     var context = get_data_from_markdown(page);
-        //     trace(filename);
-        //     Generator.output_page(context, contentPath + (filename == 'assets/content/index.html' ? 'layout/index.mtt' : 'layout/page.mtt'), 'output/' + filename);
-        // }
-
-        // TODO:
-        // Clean output
-        // Copy include dir
-
-        var portfolio_data = haxe.Json.parse(sys.io.File.getContent('assets/portfolio/portfolio.json'));
+        var portfolio_data = haxe.Json.parse(sys.io.File.getContent('portfolio/content/portfolio.json'));
         var projects :Array<Dynamic> = portfolio_data.projects;
         trace('Building ${projects.length} projects');
         for (project in projects) {
             var title :String = project.title;
             var output_filename = title.trim().replace(' ', '-').toLowerCase() + '.html';
-            Generator.output_page(project, 'assets/layout/page.mtt', 'output/pages/$output_filename');
+            Generator.output_page(project, 'portfolio/layout/page.mtt', 'portfolio/pages/$output_filename');
         }
     }
-
-    // static function get_data_from_markdown(page :Generator.Page) :Dynamic {
-    //     var headerRegEx = ~/<h1>(.*?)<\/h1>/;
-    //     if (page.data != null) validate_data(page.data);
-
-    //     return {
-    //         title: (headerRegEx.match(page.content) ? headerRegEx.matched(1) : 'Untitled'),
-    //         content: ~/&lt;!--([\s\S]*?)-->/gm.replace(page.content, ''),
-    //         data: page.data
-    //     };
-    // }
-
-    // static function validate_data(data :Dynamic) {
-    //     if (data.tags == null) throw 'Tags missing from data';
-    //     if (data.year == null) throw 'Year missing from data';
-    // }
 }
