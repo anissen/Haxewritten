d13e75e2516c59994f0dfd6dc689bb0941a49030
Anders Nissen <andnis@gmail.com>
2016-09-29 20:32:11 +0200

    Added a life in a year

--- build_watch.sh
+++ build_watch.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
-# fswatch -o assets | xargs -n1 -I{} ./build.sh # WORKS
 haxe build.hxml
 neko Haxewritten.n
-# ./build-site.sh
+
 echo "Watching..."
-# fswatch -o assets | xargs -n1 -I{} haxe build.hxml
-fswatch -o assets | xargs -n1 -I{} neko Haxewritten.n
-# fswatch -o assets | xargs -n1 -I{} ./build-site.sh
+fswatch -o portfolio | xargs -n1 -I{} neko Haxewritten.n
