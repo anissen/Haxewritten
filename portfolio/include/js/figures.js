66f8f3948420a6e24a169f5084eea6ef860034bd
Anders Nissen <andnis@gmail.com>
2016-10-26 22:51:20 +0200

    Work on index page

--- portfolio/include/js/figures.js
+++ portfolio/include/js/figures.js
@@ -1,5 +1,12 @@
 var colors = ['#001F3F', '#0074D9', '#7FDBFF', '#39CCCC', '#3D9970', '#2ECC40', '#01FF70', '#FFDC00', '#FF851B', '#FF4136', '#F012BE', '#B10DC9', '#85144B'];
+
+var seed = 2;
+function random() {
+    var x = Math.sin(seed++) * 10000;
+    return x - Math.floor(x);
+}
+
 var elements = document.getElementsByTagName('figure');
 for (var i = 0; i < elements.length; i++) {
-  elements[i].setAttribute('style', 'background-color:' + colors[Math.floor(Math.random() * colors.length)] + ';');
+  elements[i].setAttribute('style', 'background-color:' + colors[Math.floor(random() * colors.length)] + ';');
 }
--- src/Main.hx
+++ src/Main.hx
@@ -6,14 +6,16 @@ class Main {
     public static function main() {
         Generator.initialize_templates('portfolio/layout/', ~/[.]mtt$/);
 
-
         var portfolio_data = haxe.Json.parse(sys.io.File.getContent('portfolio/content/portfolio.json'));
         var projects :Array<Dynamic> = portfolio_data.projects;
         trace('Building ${projects.length} projects');
         for (project in projects) {
             var title :String = project.title;
-            var output_filename = title.trim().replace(' ', '-').toLowerCase() + '.html';
-            Generator.output_page(project, 'portfolio/layout/page.mtt', 'portfolio/pages/$output_filename');
+            project.filename = title.trim().replace(' ', '-').toLowerCase() + '.html';
+            Generator.output_page(project, 'portfolio/layout/page.mtt', 'portfolio/pages/${project.filename}');
         }
+
+        // create index
+        Generator.output_page({ projects: projects }, 'portfolio/layout/index.mtt', 'portfolio/index/index.html');
     }
 }
